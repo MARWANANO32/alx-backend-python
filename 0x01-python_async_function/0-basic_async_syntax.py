6b73821aae4e02705653c6ac28df4fd7a7574e1e
MARWANANO32 <marwanano32@gmail.com>
2024-08-07 21:08:59 +0300

    MARWAN MOSTAFA

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# this program for basic async syntax
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
